32f5d6bf1d7ed9f0a9ac42bc37a96e3cb2f4e46e
Dan2Clouted <danielfoster539@gmail.com>
2025-02-04 16:30:50 +0000

    Search Functionality WORKS!!!

---
+++ Danime/Controllers/AnimeController.cs
@@ -0,0 +1,30 @@
+﻿using Danime.Models;
+using Danime.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Danime.Controllers
+{
+    public class AnimeController : Controller
+    {
+        private readonly AnimeService _animeService;
+
+        public AnimeController(AnimeService animeService)
+        {
+            _animeService = animeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Search(string search)
+        {
+            if (string.IsNullOrEmpty(search))
+            {
+                return View();
+            }
+
+            AnimeData.Root searchResults = (await _animeService.SearchAnime(search)); 
+            return View(searchResults);
+        }
+
+
+    }
+}
---
+++ Danime/Service/AnimeService.cs
@@ -0,0 +1,38 @@
+﻿using Danime.Models;
+using Newtonsoft.Json;
+
+namespace Danime.Service
+{
+    public class AnimeService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AnimeService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<AnimeData.Root> SearchAnime(string query)
+        {
+            try
+            {
+                Console.WriteLine($"Searching for anime: {query}"); 
+
+                var response = await _httpClient.GetStringAsync($"https://api.jikan.moe/v4/anime?q={query}");
+                Console.WriteLine($"API Response: {response}"); 
+
+                var searchResult = JsonConvert.DeserializeObject<AnimeData.Root>(response);
+
+                return searchResult;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching anime: {ex.Message}");
+                return new AnimeData.Root(); // Return an empty list if an error occurs
+            }
+        }
+
+
+
+
+    }
+}
