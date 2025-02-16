103bb2ba95cf4539b407040e842a135fafe329a4
Daniel Aderibigbe <danieladeribigbe07@gmail.com>
2025-02-16 20:31:53 +0000

    Database updated

---
+++ Danime/Migrations/20250216203111_EverythingComplete.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Danime.Migrations
+{
+    /// <inheritdoc />
+    public partial class EverythingComplete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
